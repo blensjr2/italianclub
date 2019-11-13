83648fe7ac018a391f68d4cd095ebbff11d34e52
Glitch (italianclub) <none>
2019-11-13 15:15:11 +0000

    🐪👣 Checkpoint ./server.js:6679831/2355:6793142/33

--- server.js
+++ server.js
@@ -13,12 +13,13 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static("public"));
 
-// init sqlite db
+/*
 var fs = require("fs");
 var dbFile = "./.data/sqlite.db";
 var exists = fs.existsSync(dbFile);
 var sqlite3 = require("sqlite3").verbose();
 var db = new sqlite3.Database(dbFile);
+*/
 
 // init nunjucks
 const nunjucks = require('nunjucks')
@@ -26,6 +27,17 @@ nunjucks.configure('views', {
   autoescape: true,
   express: app
 })
+
+// init nodemailer
+var nodemailer = require('nodemailer');
+
+var transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'mikedough213@gmail.com',
+    pass: 'ilziwuebgmpolvrq'
+  }
+});
 
 
 // http://expressjs.com/en/starter/basic-routing.html
@@ -53,6 +65,26 @@ app.post("/register", function(req, res) {
   }
   
   console.log(JSON)
+
+  var mailOptions = {
+    from: 'mikedough213@gmail.com',
+    to:   'mikedough213@gmail.com',
+    subject: 'New Registrant',
+    text: `Email: ${email}\n
+           Grade: ${grade}\n
+           Explanation:\n\t${why}
+          `
+  };
+
+  transporter.sendMail(mailOptions, function(error, info){
+    if (error) {
+      console.log(error);
+    } else {
+      console.log('Email sent: ' + info.response);
+    }
+  });
+  
+  res.redirect("/")
 })
 
 app.get("/contact", function(request, response) {
